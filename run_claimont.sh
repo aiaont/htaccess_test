3e0e3dc0128551350fcbe3e6869cf5a19bf64c63
christiaanpauw <christiaan.pauw@nova.org.za>
2025-08-05 14:24:02 +0200

    add scripts

---
+++ run_claimont.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+   # Stop and remove any existing container with the same name
+   docker stop my-apache 2>/dev/null
+   docker rm my-apache 2>/dev/null
+
+   # Run the Docker container with volume mapping
+   docker run -d -p 8080:80 \
+     -v $(pwd)/httpd.conf:/usr/local/apache2/conf/httpd.conf \
+     -v ~/claimont/htdocs:/usr/local/apache2/htdocs \
+     --name my-apache htaccess_test
