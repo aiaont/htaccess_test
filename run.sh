f0e4dae27920cd865bf1e576e71f8afd91a67629
christiaanpauw <christiaan.pauw@nova.org.za>
2025-08-26 16:55:01 +0200

    SSL in httpsconf

--- run.sh
+++ run.sh
@@ -6,12 +6,13 @@
    docker stop my-apache 2>/dev/null
    docker rm my-apache 2>/dev/null
 
-   # Run the Docker container with volume mapping
-   docker run -d -p 8080:80 \
+   # Run the Docker container with volume mapping for both HTTP and HTTPS
+   docker run -d -p 80:80 -p 443:443 \
      -v $(pwd)/httpd.conf:/usr/local/apache2/conf/httpd.conf \
      -v $(pwd)/htdocs:/usr/local/apache2/htdocs \
      -v $(pwd)/aiao:/usr/local/apache2/htdocs/aiao \
      -v $(pwd)/claimont:/usr/local/apache2/htdocs/claimont \
      -v $(pwd)/impactont:/usr/local/apache2/htdocs/impactont \
      -v $(pwd)/infocomm:/usr/local/apache2/htdocs/infocomm \
+     -v /etc/letsencrypt:/etc/letsencrypt:ro \
      --name my-apache htaccess_test
